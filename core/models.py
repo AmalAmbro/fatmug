c8c48ed9a7dca3fe1543ceb75daea5423e4f682c
Amal Ambrose <amalambrose555@gmail.com>
2024-05-01 11:02:32 +0530

    nullable fields

--- core/models.py
+++ core/models.py
@@ -8,10 +8,10 @@ class Vendor(models.Model):
     contact_details = models.TextField()
     address = models.TextField()
     vendor_code = models.CharField(max_length=128, unique=True)
-    on_time_delivery_rate = models.FloatField()
-    quality_rating_avg = models.FloatField()
-    average_response_time = models.FloatField()
-    fulfillment_rate = models.FloatField()
+    on_time_delivery_rate = models.FloatField(null=True, blank=True)
+    quality_rating_avg = models.FloatField(null=True, blank=True)
+    average_response_time = models.FloatField(null=True, blank=True)
+    fulfillment_rate = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.name
